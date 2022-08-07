b4d282debfabb707dfc36c6f77ea66a049c77ccc
Musawer Khan Fatih <63827449+musawerfatih@users.noreply.github.com>
2022-08-07 10:19:50 +0500

    Update ytd.py

--- ytd.py
+++ ytd.py
@@ -1,41 +1,62 @@
 from pytube import YouTube
 
 
-input_link = input('Enter Link of the video: ')
+while True:
+    while True:
+        input_link = input('Enter Link of the video: ')
 
-try:
-    link = YouTube(input_link)
-except:
-    print('Connection Error')
-    quit()
+        try:
+            link = YouTube(input_link)
+            break
+        except:
+            print('Connection Error, Try again.')
+            
 
-print('\nEnter Format')
-print('1. Video Format')
-print('2. Audio Format')
+    print('\nEnter Format')
+    print('1. Video Format')
+    print('2. Audio Format')
 
-frmt = int(input('\nChoose Format: '))
-print('\nLoading available resolutions...\n')
+    frmt = int(input('\nChoose Format: '))
 
-if frmt == 1:
-    videos = link.streams.filter(file_extension='mp4')
-    vid = list(enumerate(videos))
-    for i in vid:
-        print(i)
-    print()
-    strm = int(input('Choose Resulotion (0,1,2,...): '))
-    ttle = link.title
-    print('\nDownloading...')
-    videos[strm].download()
-    print('Download Completed. ',"\"", ttle, "\"")
+    try:
+        print('\nLoading available resolutions...\n')
 
-elif frmt == 2:
-    videos = link.streams.filter(only_audio=True)
-    vid = list(enumerate(videos))
-    for i in vid:
-        print(i)
-    print()
-    strm = int(input('Choose Resulotion (0,1,2,...): '))
-    ttle = link.title
-    print('\nDownloading...',)
-    videos[strm].download()
-    print('Download Completed. ',"\"", ttle, "\"")
+        if frmt == 1:
+            videos = link.streams.filter(file_extension='mp4')
+            vid = list(enumerate(videos))
+            for i in vid:
+                print(i)
+            print()
+            strm = int(input('Choose Resulotion (0,1,2,...): '))
+            ttle = link.title
+            print('\nDownloading...')
+            videos[strm].download()
+            print('Download Completed. ',"\"", ttle, "\"")
+
+           
+            strt = input('Whould you like to download another video?(y/n): ')
+            if strt == 'n' or strt == 'N':
+                break
+            elif strt == 'y' or 'Y':
+                pass
+
+        elif frmt == 2:
+            videos = link.streams.filter(only_audio=True)
+            vid = list(enumerate(videos))
+            for i in vid:
+                print(i)
+            print()
+            strm = int(input('Choose Resulotion (0,1,2,...): '))
+            ttle = link.title
+            print('\nDownloading...',)
+            videos[strm].download()
+            print('Download Completed. ',"\"", ttle, "\"")
+
+            strt = input('Whould you like to download another video?(y/n): ')
+            if strt == 'n' or strt == 'N':
+                break
+            elif strt == 'y' or 'Y':
+                pass
+
+    except:
+        print('Video not found, Try again\n')
